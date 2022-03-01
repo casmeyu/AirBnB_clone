fc135f8315ff296b5079ff657765b65571d8fcfd
ojo-perezoso <bymartin24@gmail.com>
2022-03-01 09:24:21 -0800

    ADD: prototypes of object manipulation for console

--- console.py
+++ console.py
@@ -9,12 +9,53 @@ import cmd
 class HBTNCommand(cmd.Cmd):
     prompt = '(hbtn) '
 
+    # object manipulation
+    def do_create(self, line):
+        """Creates an instance of the desired class
+        Usage: create <class_name>
+        """
+        print(f"creating {line}")
+
+    def do_show(self, line):
+        """Prints string representation of an instance of a class based on id
+Usage: show <class_name> <id>
+        """
+        print(f"show {line}")
+
+    def do_destroy(self, line):
+        """Deletes an instance of a class based on its id
+and updates the data base
+Usage: destroy <class_name> <id>
+        """
+        print(f'destroying {line}')
+
+    def do_all(self, line):
+        """Prints the string representation of all the instances of a class
+if no class is given it prints all the instances
+Usage: all ?<class_name>?
+        """
+        if not line:
+            print("show all")
+        else:
+            print(f"all {line}")
+
+    def do_update(self, line):
+        """Updates instance of a class based on id adding/updating attributes
+only one attribute can be updated at a time
+Usage: update <class_name> <id> <attribute name> "<attribute value>"
+        """
+        print(f"update {line}")
+
     def do_quit(self, command):
-        """Quit command to exit the program"""
+        """Quit command to exit the program
+Usage: quit
+        """
         return True
 
     def do_EOF(self, command):
-        """End of File exits the program"""
+        """End of File exits the program
+Usage: EOF
+        """
         return True
 
     def emptyline(self):
