1259a2f864070ab8d9544a55fe524ae4b19c294c
camia97 <3962@holbertonschool.com>
2022-03-02 08:24:52 -0800

    add class: state amenity and review

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for class Amenity"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity model for the AirBnB app"""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module class State"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State model for AirBnB app"""
+    name = ""
