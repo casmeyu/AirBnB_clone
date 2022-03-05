e2a295d7c99638b00314336e06127844cf79dc50
camia97 <3962@holbertonschool.com>
2022-03-05 05:09:08 -0800

    change all class atributes

--- models/city.py
+++ models/city.py
@@ -5,8 +5,8 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """City Model for AirBnB"""
+    state_id = ""
+    name = ""
     def __init__(self, *args, **kwargs):
         """Initialization ofcity model"""
-        self.state_id = ""
-        self.name = ""
         super().__init__(*args, **kwargs)
--- models/review.py
+++ models/review.py
@@ -5,9 +5,9 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """Review model for the AirBnB app"""
+    place_id = ""
+    user_id = ""
+    text = ""
     def __init__(self, *args, **kwargs):
         """Initialization of review model"""
-        self.place_id = ""
-        self.user_id = ""
-        self.text = ""
         super().__init__(*args, **kwargs)
