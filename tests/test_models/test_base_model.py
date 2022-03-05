6dcdcae076d4117f289fd8b17005df8315dc0cca
ojo-perezoso <bymartin24@gmail.com>
2022-03-05 06:59:10 -0800

    add: more test for base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for testing the Base Model"""
 import unittest
+from datetime import datetime
 from models.base_model import BaseModel
 
 
@@ -11,10 +12,14 @@ class BaseModelTest(unittest.TestCase):
         new_bm = BaseModel()
         self.assertEqual(type(new_bm), BaseModel)
         self.assertEqual(type(new_bm.id), str)
+        self.assertEqual(type(new_bm.created_at), datetime)
+        self.assertEqual(type(new_bm.updated_at), datetime)
         self.assertEqual(type(new_bm.to_dict()), dict)
+        self.assertEqual(type(new_bm.__str__()), str)
 
     def test_documentation(self):
         """Test the documentation for BaseModel"""
         new_bm = BaseModel()
         self.assertNotEqual(len(new_bm.__doc__), 0)
-
+        self.assertNotEqual(len(new_bm.save.__doc__), 0)
+        self.assertNotEqual(len(new_bm.to_dict.__doc__), 0)
