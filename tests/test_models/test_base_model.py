ac869c92aea89423a8a351b993588a4c1d45ad33
ojo-perezoso <bymartin24@gmail.com>
2022-03-05 07:09:46 -0800

    add: more test for BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@
 import unittest
 from datetime import datetime
 from models.base_model import BaseModel
+from models import storage
 
 
 class BaseModelTest(unittest.TestCase):
@@ -14,8 +15,9 @@ class BaseModelTest(unittest.TestCase):
         self.assertEqual(type(new_bm.id), str)
         self.assertEqual(type(new_bm.created_at), datetime)
         self.assertEqual(type(new_bm.updated_at), datetime)
-        self.assertEqual(type(new_bm.to_dict()), dict)
-        self.assertEqual(type(new_bm.__str__()), str)
+
+        new_bm = BaseModel(**{'name': "Cami"})
+        self.assertEqual(new_bm.name, "Cami")
 
     def test_documentation(self):
         """Test the documentation for BaseModel"""
@@ -23,3 +25,16 @@ class BaseModelTest(unittest.TestCase):
         self.assertNotEqual(len(new_bm.__doc__), 0)
         self.assertNotEqual(len(new_bm.save.__doc__), 0)
         self.assertNotEqual(len(new_bm.to_dict.__doc__), 0)
+
+    def test_methods(self):
+        """Test the methods of BaseModel"""
+        new_bm = BaseModel()
+        self.assertEqual(type(new_bm.to_dict()), dict)
+        self.assertEqual(type(new_bm.__str__()), str)
+
+        last_update = new_bm.updated_at
+        new_bm.save()
+        key = f'BaseModel.{new_bm.id}'
+        self.assertEqual(new_bm, storage.all()[key])
+        self.assertNotEqual(new_bm.updated_at, last_update)
+
