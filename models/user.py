9dfae81452dd9332e502e089e8ba6c0db32902fb
camia97 <3962@holbertonschool.com>
2022-03-05 05:02:12 -0800

    change: class atributes

--- models/user.py
+++ models/user.py
@@ -5,10 +5,10 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User model for the AirBnB application"""
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
     def __init__(self, *args, **kwargs):
         """Initialization for the user model"""
         super().__init__(*args, **kwargs)
