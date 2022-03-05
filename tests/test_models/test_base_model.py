4fc66ad0786a558dc3d5283fc2c15071aaaba1a3
ojo-perezoso <bymartin24@gmail.com>
2022-03-05 06:54:59 -0800

    add: test for documentation of BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,6 +7,14 @@ from models.base_model import BaseModel
 class BaseModelTest(unittest.TestCase):
     """Class for testing the Base Model"""
     def test_creation(self):
+        """Test the creation and type of attributes of BaseModel"""
         new_bm = BaseModel()
         self.assertEqual(type(new_bm), BaseModel)
+        self.assertEqual(type(new_bm.id), str)
         self.assertEqual(type(new_bm.to_dict()), dict)
+
+    def test_documentation(self):
+        """Test the documentation for BaseModel"""
+        new_bm = BaseModel()
+        self.assertNotEqual(len(new_bm.__doc__), 0)
+
