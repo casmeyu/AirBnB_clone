c31e48ee0009da06d290606208a6ae18d8298f53
camia97 <3962@holbertonschool.com>
2022-03-04 22:27:27 -0800

    broken user

--- models/user.py
+++ models/user.py
@@ -7,8 +7,8 @@ class User(BaseModel):
     """User model for the AirBnB application"""
     def __init__(self, *args, **kwargs):
         """Initialization for the user model"""
+        super().__init__(*args, **kwargs)
         self.email = ""
         self.password = ""
         self.first_name = ""
         self.last_name = ""
-        super().__init__(*args, **kwargs)
